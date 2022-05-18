0dd8f8ea4fe026afcce471d7bb316ed0e6aa7fe3
Abdul Razzaque Goilkar <93587845+BackendRaza@users.noreply.github.com>
2022-05-18 17:54:18 +0400

    New product page in BS5 and Vue

---
+++ ProductPage/main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            title:'Product Page',
+            prodName:'Nice Socks',
+            desc:'Long lasting socks!! you will love it',
+            features: ['Polyster','10 Days return policy','One Year warranty']
+        }
+    }
+})
