084be60d5b2edfc2aeaa782feab17e3285d02319
Abdul Razzaque Goilkar <93587845+BackendRaza@users.noreply.github.com>
2022-05-18 15:27:58 +0400

    changed  the rating div to card

--- components/reviewList.js
+++ components/reviewList.js
@@ -7,16 +7,18 @@ app.component('review-list', {
     },
     template: 
     /*html*/
-    `<div class="review-container">
-    <h3>Reviews:</h3>
-      <ul>
-        <li v-for="(review, index) in reviews" :key="index">
-          {{ review.name }} gave this {{ review.rating }} stars
-          <br/>
-          "{{ review.review }}"
-          <br/>
-        </li>
-      </ul>
+    `
+    <div class="row container">   
+    <div v-for="(review, index) in reviews" :key="index" class="col-md-3 card text-bg-info m-3">
+        <div class="card-body">
+            <h5 class="card-title">{{ review.name }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Ratings</h6>
+            <p class="card-text">{{ review.rating }}</p>
+            <p>Review: {{review.review}}</p>
+        </div>
+    </div>
     </div>`,
     
 })
+
+
